54b506ed3fedaef636612709d209a4b43e95c77e
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2021-04-22 09:33:13 +0200

    Add rust-analyzer to the toolchain

--- rustup-toolchain.sh
+++ rustup-toolchain.sh
@@ -33,7 +33,7 @@ fi
 
 # Install and setup new toolchain.
 rustup toolchain uninstall taint
-rustup-toolchain-install-master -n taint -c rust-src -c rustc-dev -c llvm-tools -c rustfmt -- "$NEW_COMMIT"
+rustup-toolchain-install-master -n taint -c rust-src -c rustc-dev -c llvm-tools -c rustfmt -c rust-analyzer -- "$NEW_COMMIT"
 rustup override set taint
 
 # Cleanup.
