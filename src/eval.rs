e90287a0039cf7eceac8ce88c3f54b624ac897fd
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2021-04-23 09:54:35 +0200

    Hook into analysis

--- src/eval.rs
+++ src/eval.rs
@@ -1,5 +1,8 @@
 use rustc_hir::def_id::DefId;
 use rustc_middle::ty::TyCtxt;
+use rustc_mir::dataflow::Analysis;
+
+use crate::analysis::MaybeTaintedLocals;
 
 pub struct TaintConfig {
     pub ownership: bool,
@@ -16,5 +19,18 @@ pub fn eval_main<'tcx>(tcx: TyCtxt<'tcx>, main_id: DefId, config: TaintConfig) -
     let _ = main_id;
     let _ = config;
 
+    let body = tcx.optimized_mir(main_id);
+
+    let analysis = MaybeTaintedLocals {};
+    let mut results = analysis
+        .into_engine(tcx, body)
+        .iterate_to_fixpoint()
+        .into_results_cursor(body);
+
+    for (bb, _) in body.basic_blocks().iter_enumerated() {
+        results.seek_before_primary_effect(body.terminator_loc(bb));
+        let state = results.get();
+        dbg!(state);
+    }
     Some(0)
 }
