8f7af2567f6a34f667f4d29b8fb575b8422f850c
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2021-05-27 13:02:07 +0200

    fix formatting

--- src/bins/callback_impls.rs
+++ src/bins/callback_impls.rs
@@ -10,7 +10,7 @@ use std::convert::TryFrom;
 
 /// Perform the taint analysis.
 pub fn mir_analysis(tcx: rustc_middle::ty::TyCtxt) {
-        let (entry_def_id, _) = if let Some((entry_def, x)) = tcx.entry_fn(LOCAL_CRATE) {
+    let (entry_def_id, _) = if let Some((entry_def, x)) = tcx.entry_fn(LOCAL_CRATE) {
         (entry_def, x)
     } else {
         err_msg(
@@ -29,4 +29,3 @@ pub fn mir_analysis(tcx: rustc_middle::ty::TyCtxt) {
 fn err_msg(msg: &str) -> ! {
     rustc_session::early_error(ErrorOutputType::default(), msg);
 }
-
