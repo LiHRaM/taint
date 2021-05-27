fb61e0cab334fd8c58cf14377a1a9f5401610ef1
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2021-05-27 13:38:25 +0200

    Add tests for Attribute Finder and fix it too

--- src/eval.rs
+++ src/eval.rs
@@ -85,15 +85,12 @@ fn fun_name() -> AttrInfo {
     }
 }
 
-impl<'v> ItemLikeVisitor<'v> for TaintAttributeFinder<'_> {
-    fn visit_item(&mut self, item: &'v rustc_hir::Item<'_>) {
-        let item_id = item.hir_id();
+impl TaintAttributeFinder<'_> {
+    fn visit_hir_id(&mut self, item_id: hir::HirId) {
         let def_id = self.tcx.hir().local_def_id(item_id).to_def_id();
-
         let sym_source = Symbol::intern("source");
         let sym_sink = Symbol::intern("sink");
         let sym_sanitizer = Symbol::intern("sanitizer");
-
         let attrs = self.tcx.hir().attrs(item_id);
         for attr in attrs {
             if let AttrKind::Normal(ref item, _) = attr.kind {
@@ -110,18 +107,29 @@ impl<'v> ItemLikeVisitor<'v> for TaintAttributeFinder<'_> {
                             span: item.span(),
                         })
                     }
-
                     break;
                 }
             }
         }
     }
+}
 
-    fn visit_trait_item(&mut self, _trait_item: &rustc_hir::TraitItem<'_>) {}
+impl<'v> ItemLikeVisitor<'v> for TaintAttributeFinder<'_> {
+    fn visit_item(&mut self, item: &'v rustc_hir::Item<'_>) {
+        self.visit_hir_id(item.hir_id());
+    }
 
-    fn visit_impl_item(&mut self, _impl_item: &rustc_hir::ImplItem<'_>) {}
+    fn visit_trait_item(&mut self, trait_item: &rustc_hir::TraitItem<'_>) {
+        self.visit_hir_id(trait_item.hir_id());
+    }
 
-    fn visit_foreign_item(&mut self, _foreign_item: &rustc_hir::ForeignItem<'_>) {}
+    fn visit_impl_item(&mut self, impl_item: &rustc_hir::ImplItem<'_>) {
+        self.visit_hir_id(impl_item.hir_id());
+    }
+
+    fn visit_foreign_item(&mut self, foreign_item: &rustc_hir::ForeignItem<'_>) {
+        self.visit_hir_id(foreign_item.hir_id());
+    }
 }
 
 fn get_taint_attr(item: &rustc_ast::AttrItem) -> Option<&Symbol> {
